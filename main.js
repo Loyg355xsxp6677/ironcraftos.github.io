acb63ab7fcbcb18fd206efb445190a1ee1fee876
Loyg355xsxp6677 <154547706+Loyg355xsxp6677@users.noreply.github.com>
2025-04-26 19:54:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("joinButton");
+  btn.addEventListener("click", () => {
+    alert("انضم الآن إلى سيرفر IronCraftOS!\nIP: قيد العمل");
+  });
+});
